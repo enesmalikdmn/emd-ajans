a8f681d8e6f4f8073dde2e47cacd724622bbed0c
enesmalikdmn <m.duman@digitastic.app>
2025-03-23 17:43:30 +0300

    feat: Enhance Header component and add client-side imports to page.tsx

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import Link from 'next/link';
+import Image from 'next/image';
 import { Button } from '@mui/material';
 
 const Header = () => {
@@ -8,9 +9,14 @@ const Header = () => {
     <header className="h-1/12 flex items-center fixed w-full bg-transparent z-50">
       <nav className="container mx-auto px-4 py-4">
         <div className="flex items-center justify-between">
-          <Link href="/" className="text-[20px] font-normal">
-            <span className="text-[#fff]">emd</span>
-            <span className="text-[#4A90E2]">Ajans</span>
+          <Link href="/" className="relative w-[180px] h-[120px]">
+            <Image
+              src="/images/logo.png"
+              alt="EMD Ajans Logo"
+              fill
+              className="object-contain"
+              priority
+            />
           </Link>
           
           <div className="flex items-center space-x-8">
